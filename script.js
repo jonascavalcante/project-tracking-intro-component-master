9a63f1f4f4c519e20234de30466c4f3340a48d82
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2020-12-31 00:12:54 -0300

    Resolvido bug do menu mobile

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const menuToggle = () => {
-    const menu = document.querySelector('.menu');
-    const open = document.querySelector('.open');
-    const close = document.querySelector('.close');
-    const nav = document.querySelector('nav');
 
-    menu.addEventListener('click', () => {
-        open.classList.add('disable');
-        close.classList.add('active');
-        nav.classList.add('menu-opened');
-    });
+function menuToggle() {
+
+    let menuMobile = document.querySelector('nav');
+    
+    if (menuMobile.style.display == 'none') {
+        menuMobile.style.display = 'flex';
+    } else {
+        menuMobile.style.display = 'none';
+    }
 }
-
-menuToggle();
