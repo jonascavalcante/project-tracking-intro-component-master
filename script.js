4d6dd2a7af9df62e7fae23879910de1c2ffc31ba
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2021-01-02 15:33:57 -0300

    Ajuste no menu mobile e no padding inferior

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function menuToggle() {
 
     let menuMobile = document.querySelector('nav');
     
-    if (menuMobile.style.display == 'none') {
+    if (menuMobile.style.display == '' || menuMobile.style.display == 'none') {
         menuMobile.style.display = 'flex';
     } else {
         menuMobile.style.display = 'none';
