f75cb110955a93b5363e3547c413249144d13289
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2021-01-03 11:20:03 -0300

    Ajuste no botão de menu

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 function menuToggle() {
 
     let menuMobile = document.querySelector('nav');
+    let buttonOpen = document.querySelector('.open');
+    let buttonClose = document.querySelector('.close');
     
     if (menuMobile.style.display == '' || menuMobile.style.display == 'none') {
         menuMobile.style.display = 'flex';
+        buttonClose.style.display = 'block';
+        buttonOpen.style.display = 'none';
     } else {
         menuMobile.style.display = 'none';
+        buttonClose.style.display = 'none';
+        buttonOpen.style.display = 'block';
     }
 }
